dbfa72428f6619bfb9218ba932cbfac43deb3963
Diego Ceccacci <diegoceccacci92@gmail.com>
2021-05-20 17:46:30 +0200

    more tests

--- FluentBoolean.Test/ComplexExpressions/ComplexExpressionsTest.cs
+++ FluentBoolean.Test/ComplexExpressions/ComplexExpressionsTest.cs
@@ -62,5 +62,32 @@ namespace FluentBoolean.Test.ComplexExpressions
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestCase(false, false, false, false)]
+        [TestCase(false, false, false, true)]
+        [TestCase(false, false, true, false)]
+        [TestCase(false, false, true, true)]
+        [TestCase(false, true, false, false)]
+        [TestCase(false, true, false, true)]
+        [TestCase(false, true, true, false)]
+        [TestCase(false, true, true, true)]
+        [TestCase(true, false, false, false)]
+        [TestCase(true, false, false, true)]
+        [TestCase(true, false, true, false)]
+        [TestCase(true, false, true, true)]
+        [TestCase(true, true, false, false)]
+        [TestCase(true, true, false, true)]
+        [TestCase(true, true, true, false)]
+        [TestCase(true, true, true, true)]
+        public void Expression4_ShouldBehaveAsExpected(bool par1, bool par2, bool par3, bool par4)
+        {
+            var expected = par1 || par2 || par3 || par4;
+            var actual = par1
+                .OR(par2)
+                .OR(par3)
+                .OR(par4);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
