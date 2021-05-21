1e0d620f03b54b48fbf1dd8d921fe031c9abee7b
Diego Ceccacci <diegoceccacci92@gmail.com>
2021-05-21 13:04:27 +0200

    csproj

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentBoolean;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public void Test()
+        {
+            var fluentResult = true
+                .OR(false)
+                .OR(true)
+                .OR(true);
+
+            var classicResult = true || false || true || true;
+
+            //fluentResult and classicResult produce the same result
+        }
+    }
+}
